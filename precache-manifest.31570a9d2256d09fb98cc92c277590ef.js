47c1180cd914c6c5bc546479e10eb492dffdaca3
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-10-07 12:42:36 +0530

    Updates

---
+++ precache-manifest.31570a9d2256d09fb98cc92c277590ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47b6261ef496d22ee9e771cc7a4e2b0",
+    "url": "/anonyspace/index.html"
+  },
+  {
+    "revision": "ac61e5b7aa0e25e55a7e",
+    "url": "/anonyspace/static/css/main.0a4a04a6.chunk.css"
+  },
+  {
+    "revision": "b9270d170113ef4bb739",
+    "url": "/anonyspace/static/js/2.b873857b.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/anonyspace/static/js/2.b873857b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac61e5b7aa0e25e55a7e",
+    "url": "/anonyspace/static/js/main.7371b9d9.chunk.js"
+  },
+  {
+    "revision": "9d04ce4bb854a9153f08",
+    "url": "/anonyspace/static/js/runtime-main.7d12d028.js"
+  }
+]);
